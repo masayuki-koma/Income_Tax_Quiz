07dd2dd8d006567f45e5e2e43c033f8218ccbd67
masayuki-koma <paddington.masa@gmail.com>
2020-08-17 12:25:47 +0900

    activityの遷移の実装

--- app/src/main/java/com/asamjapan/incometaxquiz/MainActivity.kt
+++ app/src/main/java/com/asamjapan/incometaxquiz/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.asamjapan.incometaxquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cardRandom.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
